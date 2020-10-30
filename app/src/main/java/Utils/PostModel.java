18e54d47d7977fb615b5adbd5c0205f652e387a0
aditya <aditya84850@gmail.com>
2020-10-30 14:02:31 +0530

    added firestore connection and bug fixes

---
+++ app/src/main/java/Utils/PostModel.java
@@ -0,0 +1,83 @@
+package Utils;
+
+public class PostModel {
+    private String uid, imageUrl, postTitle, postDescription, postId, timestamp, likes, userName;
+
+    public PostModel() {
+    }
+
+    public PostModel(String uid, String imageUrl, String postTitle, String postDescription, String postId, String timestamp, String likes, String userName) {
+        this.uid = uid;
+        this.imageUrl = imageUrl;
+        this.postTitle = postTitle;
+        this.postDescription = postDescription;
+        this.postId = postId;
+        this.timestamp = timestamp;
+        this.likes = likes;
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getPostTitle() {
+        return postTitle;
+    }
+
+    public void setPostTitle(String postTitle) {
+        this.postTitle = postTitle;
+    }
+
+    public String getPostDescription() {
+        return postDescription;
+    }
+
+    public void setPostDescription(String postDescription) {
+        this.postDescription = postDescription;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getLikes() {
+        return likes;
+    }
+
+    public void setLikes(String likes) {
+        this.likes = likes;
+    }
+}
--- app/src/main/java/com/example/myapplication/login_signup_getstarted.java
+++ app/src/main/java/com/example/myapplication/login_signup_getstarted.java
@@ -1,5 +1,7 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,23 +11,82 @@ import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import Utils.UserApi;
 
 public class login_signup_getstarted extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("Users");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        firebaseAuth=FirebaseAuth.getInstance();
-
-        //if user authenticated and profile is created go to main activity
-        FirebaseUser currentUser=firebaseAuth.getCurrentUser();
-        if(currentUser != null ){
-               startActivity(new Intent(login_signup_getstarted.this,MainActivity.class));
-               finish();
-        }
-
         setContentView(R.layout.activity_login_signup_getstarted);
+        firebaseAuth=FirebaseAuth.getInstance();
+        currentUser = firebaseAuth.getCurrentUser();
+        //if user authenticated and profile is created go to main activity
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                if(currentUser != null ){
+
+                    final String currentUserId = currentUser.getUid();
+
+                    collectionReference.whereEqualTo("uid", currentUserId)
+                            .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                                @Override
+                                public void onEvent(@Nullable QuerySnapshot value,
+                                                    @Nullable FirebaseFirestoreException error) {
+
+                                    if (error != null)
+                                    {
+                                        return;
+                                    }
+
+                                    String currentUserName;
+                                    if (!value.isEmpty())
+                                    {
+                                        for (QueryDocumentSnapshot snapshot:value)
+                                        {
+                                            UserApi userApi = UserApi.getInstance();
+                                            userApi.setBio(snapshot.getString("bio"));
+                                            userApi.setCover(snapshot.getString("cover"));
+                                            userApi.setEmail(snapshot.getString("email"));
+                                            userApi.setFollowerCount(snapshot.getString("follower"));
+                                            userApi.setImage(snapshot.getString("image"));
+                                            userApi.setPhone(snapshot.getString("phone"));
+                                            userApi.setProfession(snapshot.getString("profession"));
+                                            userApi.setUsername(snapshot.getString("username"));
+
+                                            startActivity(new Intent(login_signup_getstarted.this,MainActivity.class));
+                                            finish();
+                                        }
+                                    }
+
+                                }
+                            });
+                }
+                else
+                {
+                    //if he is new user
+                }
+
+            }
+        };
+
+
         Button btnsignup=findViewById(R.id.start_signup_btn);
         Button btnlogin=findViewById(R.id.start_login_btn);
         btnlogin.setOnClickListener(new View.OnClickListener() {
@@ -44,5 +105,21 @@ public class login_signup_getstarted extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        currentUser = firebaseAuth.getCurrentUser();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (firebaseAuth != null)
+        {
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
     }
 }
